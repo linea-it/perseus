cf68371ec91af743ec197a7144c2752d419df9c7
“felipelm” <felipelm123@gmail.com>
2019-03-26 09:11:45 -0300

    fixing test

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import Lokka from 'lokka';
 import Transport from 'lokka-transport-http';
 
 const apiUrl =
-  process.env.NODE_ENV !== 'development'
+  process.env.NODE_ENV === 'production'
     ? window._env_.API_URL
     : process.env.REACT_APP_API_URL;
 
